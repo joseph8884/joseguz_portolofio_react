18d991cfd65bb0edd94732cd90bef12cdce769b0
jose <josegzm08@gmail.com>
2024-10-09 20:12:55 -0500

    add three js

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,28 +4,27 @@ import { FaFigma } from "react-icons/fa";
 import Type from "./Type";
 import React from "react";
 import "./header.css";
-import Spline from '@splinetool/react-spline';
-
+import Spline from "@splinetool/react-spline";
 
 function Header() {
   return (
-  
     <header id="home">
-      
       <div className="header_container">
         <section className="whoami">
-          <h5>Hello I'm<span className="wave" role="img" aria-labelledby="wave">👋🏻</span></h5>
+          <h5>
+            Hello I'm
+            <span className="wave" role="img" aria-labelledby="wave">
+              👋🏻
+            </span>
+          </h5>
           <h1>JOSE GUZMAN</h1>
           <Type />
         </section>
-        
-                <div className="me">
-                <Spline
-        scene="https://prod.spline.design/eC4-Y8Dgukm8upuI/scene.splinecode" 
-      />
-                </div>
-        
-        
+
+        <div className="me">
+          <Spline scene="https://prod.spline.design/eC4-Y8Dgukm8upuI/scene.splinecode" />
+        </div>
+
         <div className="cta">
           <a
             href="/resume_url"
@@ -38,20 +37,26 @@ function Header() {
           <a href="/contact" className="btn btn-primary">
             Let's Talk
           </a>
-          
         </div>
         <div className="header_social">
-          <a href="https://www.linkedin.com/in/héctor-jose-guzmán-montaño-2093a2238" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.linkedin.com/in/héctor-jose-guzmán-montaño-2093a2238"
+            target="_blank"
+            rel="noreferrer"
+          >
             <BsLinkedin />
           </a>
-          <a href="https://github.com/joseph8884" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/joseph8884"
+            target="_blank"
+            rel="noreferrer"
+          >
             <BsGithub />
           </a>
           <a href="http://figma.com" target="_blank" rel="noreferrer">
             <FaFigma />
           </a>
         </div>
-        
       </div>
     </header>
   );
